88ea46883f95a87c5dce384e137848916e51d34e
Aljaz <aljaz.blazej@gmail.com>
2017-01-14 13:11:04 +0100

    Static assets minified

---
+++ backend/static/scripts/stars_min.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',main,!1);function main(){var starGroups=document.getElementsByClassName("stars_group");for(var i=0;i<starGroups.length;i++){var starCount=parseInt(starGroups[i].innerHTML);starGroups[i].innerHTML="";for(var j=0;j<starCount;j++){starGroups[i].innerHTML+='<img src="/static/images/star.png" alt="Star" class="star_icon">'}}}
